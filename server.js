0562cb0266424a648cff4434ccb3d64648496f22
gokul278 <gokulhk278@gmail.com>
2024-08-15 15:37:24 +0530

    UI URL Updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://demostockmanagement.vercel.app/"
+    origin: process.env.URL
 }));
 
 app.use("/api", LoginRoutes);
